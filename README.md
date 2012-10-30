1ea086f3060244c6d05da48ce5aa8ba8450111ac
Peter Hamilton <peter@inspiredpixel.net>
2012-10-30 06:43:27 +0000

    Added a very basic slideshare plugin

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 Octopress-Slideshare-Plugin
 ===========================
 
-Helps Embed a Slideshare Presentation in Octopress
+Helps Embed a Slideshare Presentation in Octopress.
+
+Usage
+=====
+
+- Install ```slideshare.rb``` in your ```plugins``` folder.
+- Place ```{% slideshare ID %} in your MD file
---
+++ slideshare.rb
@@ -0,0 +1,15 @@
+module Jekyll
+  class SlideShare < Liquid::Tag
+
+    def initialize(name, id, tokens)
+      super
+      @id = id
+    end
+
+    def render(context)
+      %(<iframe src="http://www.slideshare.net/slideshow/embed_code/#{@id}" width="650" height="356" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC;border-width:1px 1px 0;margin-bottom:5px" allowfullscreen></iframe> )
+    end
+  end
+end
+
+Liquid::Template.register_tag('slideshare', Jekyll::SlideShare)
