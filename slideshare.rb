94b31e59e8ebf77acc69cb952c0a9dce478d9b99
Peter Hamilton <peter@inspiredpixel.net>
2012-10-30 06:44:42 +0000

    Updated width of iframe to be narrower

--- slideshare.rb
+++ slideshare.rb
@@ -7,7 +7,7 @@ module Jekyll
     end
 
     def render(context)
-      %(<iframe src="http://www.slideshare.net/slideshow/embed_code/#{@id}" width="650" height="356" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC;border-width:1px 1px 0;margin-bottom:5px" allowfullscreen></iframe> )
+      %(<iframe src="http://www.slideshare.net/slideshow/embed_code/#{@id}" width="437" height="356" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC;border-width:1px 1px 0;margin-bottom:5px" allowfullscreen></iframe> )
     end
   end
 end
